736a1476cef076c5c23bf78432460c8b256aa740
utahisnotastate <utahisnotastate@gmail.com>
2023-07-23 22:31:14 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+import {config} from "dotenv"
+config()
+import {Configuration, OpenAIApi} from "openai";
+import readline from "readline";
+
+const openai = new OpenAIApi(new Configuration({
+    apiKey: process.env.API_KEY,
+
+}))
+
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+userInterface.prompt()
+
+userInterface.on("line", async input => {
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{role: 'user', content: input}],
+    })
+    console.log(response.data.choices[0].message.content)
+    userInterface.prompt()
+})
+
+
